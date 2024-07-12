2a69e368348a8e48c08dff43bddcdb93e6e76622
Subhasish <onlyme.karmakar@gmail.com>
2024-07-12 12:49:11 +0530

    added spec for empty and single digit char

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,8 +1,10 @@
 class Calculator
     def add(str)
+
+        return "" if str.empty?
         str_array = str.split(/[;,]/)
         integers_array = str_array.select { |item| item.match?(/^[-]?\d+$/) }.map(&:to_i)
-         
+        return integers_array[0] if  integers_array.size == 1
         integers_array.sum
     end
 end
--- spec/calculator.rb
+++ spec/calculator.rb
@@ -4,10 +4,31 @@ describe Calculator do
    subject {Calculator.new}
 
     context 'valid string without negative number' do
-        let(:str) { "2,3,4" }
+        let (:str) { "\\nr,3,5"}
         it 'when string element separated by comma' do
+            str = "2,3,4"
             expect(subject.add(str)).to eql(9)
         end
+
+        it 'when delimiter is ";"' do
+            str = "2;3;4"
+            expect(subject.add(str)).to eql(9)
+        end
+
+        it 'when string contains others chars' do
+            str =  "//;\n1;2"
+            expect(subject.add(str)).to eql(3)
+        end
+
+        it 'when string contains empty chars' do
+            str =  ""
+            expect(subject.add(str)).to eql("")
+        end
+        it 'when string contains empty only one digit' do
+            str =  "//;2"
+            expect(subject.add(str)).to eql(2)
+        end
+
     end
 
 end
