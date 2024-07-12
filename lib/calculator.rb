cb65bad312ced45a7c86598dddea1c966bc92d94
Subhasish <onlyme.karmakar@gmail.com>
2024-07-12 13:00:49 +0530

    added logic to handle negative number

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -5,6 +5,18 @@ class Calculator
         str_array = str.split(/[;,]/)
         integers_array = str_array.select { |item| item.match?(/^[-]?\d+$/) }.map(&:to_i)
         return integers_array[0] if  integers_array.size == 1
-        integers_array.sum
+        sum = 0
+        exception_message = []
+        allowed_to_calculate = true
+        integers_array.each do |n|
+
+            if n > 0 && allowed_to_calculate
+                sum += n 
+            else
+                allowed_to_calculate ||= false
+                exception_message << "negative numbers not allowed: #{n},"
+            end
+        end
+        exception_message.present? ? exception_message : sum
     end
 end
